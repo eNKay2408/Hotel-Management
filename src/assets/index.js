6a2042b25d50131d841d0bab4f2203852e38c759
eNKay <nguyenkhai24082408@gmail.com>
2024-10-24 23:14:54 +0700

    feat: add assets for icons, logos, and avatars
    - Added booking, room, report, home, invoice, and regulation icons
    - Added logo and logo background images
    - Added social media icons for Facebook, Twitter, Instagram, and LinkedIn
    - Added avatar images

---
+++ src/assets/index.js
@@ -0,0 +1,38 @@
+import bookingIcon from "./icon/booking.png";
+import roomIcon from "./icon/room.png";
+import reportIcon from "./icon/report.png";
+import homeIcon from "./icon/home.png";
+import invoiceIcon from "./icon/invoice.png";
+import regulationIcon from "./icon/regulation.png";
+
+import logo from "./logo.png";
+import logoBackground from "./logo-background.png";
+
+import facebook from "./social/facebook.png";
+import twitter from "./social/twitter.png";
+import instagram from "./social/instagram.png";
+import linkedin from "./social/linkedin.png";
+
+import avatar1 from "./avatar/1.png";
+import avatar2 from "./avatar/2.png";
+import avatar3 from "./avatar/3.png";
+import avatar4 from "./avatar/4.png";
+
+export {
+	bookingIcon,
+	roomIcon,
+	reportIcon,
+	homeIcon,
+	invoiceIcon,
+	regulationIcon,
+	logo,
+	logoBackground,
+	facebook,
+	twitter,
+	instagram,
+	linkedin,
+	avatar1,
+	avatar2,
+	avatar3,
+	avatar4,
+};
