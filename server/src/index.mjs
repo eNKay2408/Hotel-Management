dd6a2b6cc149f1d829bdb53b628557a20886bf72
Sciderus <huydska11@gmail.com>
2024-11-12 09:22:13 +0700

    add connection to sqlserver

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,13 +1,28 @@
-import express from 'express';
-import RoomRoutes from './routes/room.mjs';
-import BookingRotes from './routes/booking.mjs';
-import InvoiceRoutes from './routes/invoice.mjs';
+import express from "express";
+import RoomRoutes from "./routes/room.mjs";
+import BookingRotes from "./routes/booking.mjs";
+import InvoiceRoutes from "./routes/invoice.mjs";
+import sequelize from "./database/database.js";
 
 const app = express();
 app.use(express.json());
-app.use('/room',RoomRoutes);
-app.use('/booking',BookingRotes);
-app.use('/invoice',InvoiceRoutes);
+
+// testConnection.js
+
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+}
+
+testConnection();
+
+app.use("/room", RoomRoutes);
+app.use("/booking", BookingRotes);
+app.use("/invoice", InvoiceRoutes);
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,6 +30,6 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-    res.send('Back-end server is running');
+app.get("/", (req, res) => {
+    res.send("Back-end server is running");
 });
