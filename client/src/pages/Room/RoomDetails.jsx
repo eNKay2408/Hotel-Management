8e3e25618d595e2a34c76701f844f909ad13a036
eNKay <nguyenkhai24082408@gmail.com>
2024-11-13 01:25:07 +0700

    feat: integrate API calls for fetching and updating room details in RoomDetails component
    - Updated RoomDetails.jsx to fetch room details from the backend API instead of using sample data

--- client/src/pages/Room/RoomDetails.jsx
+++ client/src/pages/Room/RoomDetails.jsx
@@ -2,124 +2,101 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
 
 import { Title, Button } from "../../components";
-import { getRoom } from "../../services";
+import { getRoom, updateRoom } from "../../services";
 
 const RoomDetails = () => {
-	const { id } = useParams();
+  const { id: number } = useParams();
 
-	/*
-	* Get room details by id
-	* API: GET /room/:id
+  const [room, setRoom] = useState({});
 
-	const [room, setRoom] = useState({});
+  useEffect(() => {
+    const loadRoom = async () => {
+      const data = await getRoom(number);
+      setRoom(data);
+    };
+    loadRoom();
+  }, [number]);
 
-	useEffect(() => {
-		const loadRoom = async () => {
-			const data = await getRoom(id);
-			setRoom(data);
-		}
-		loadRoom();
-	}, []);
-	*/
+  const imageRef = useRef(null);
 
-	const [room, setRoom] = useState({
-		number: 101,
-		type: "A",
-		area: 500,
-		quantity: 3,
-		rate: 299,
-		image:
-			"https://res.cloudinary.com/dvzhmi7a9/image/upload/v1729681254/PlaceHolderRoom.png",
-	});
+  const handleImageUpload = () => {
+    const file = imageRef.current.files[0];
+    const reader = new FileReader();
 
-	const imageRef = useRef(null);
+    reader.onloadend = () => {
+      setRoom({ ...room, image: reader.result });
+    };
 
-	const handleImageUpload = () => {
-		const file = imageRef.current.files[0];
-		const reader = new FileReader();
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
 
-		reader.onloadend = () => {
-			setRoom({ ...room, image: reader.result });
-		};
+  const navigate = useNavigate();
 
-		if (file) {
-			reader.readAsDataURL(file);
-		}
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-	const navigate = useNavigate();
+    const response = await updateRoom(number, room);
+    const data = await response.json();
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+    if (response.ok) {
+      alert(data.message);
+      navigate("/rooms");
+    } else {
+      alert(data.message);
+      console.error(response);
+    }
+  };
 
-		const formData = new FormData(e.target);
+  return (
+    <div className="flex flex-col w-full py-4 px-2">
+      <Title title="Room Details" />
 
-		const response = await fetch(`/api/rooms/${id}`, {
-			method: "PUT",
-			body: formData,
-		});
+      <div className="mx-auto max-w-[700px] w-full">
+        <form
+          className="flex flex-col gap-4 items-center"
+          onSubmit={handleSubmit}
+        >
+          <div className="grid md:grid-cols-2 grid-cols-1 font-play gap-4">
+            {Object.entries(room)
+              .filter(([key]) => key !== "image")
+              .map(([key, value]) => (
+                <div key={key} className="flex flex-col">
+                  <label className="font-bold text-xl">
+                    {key.charAt(0).toUpperCase() + key.slice(1)}
+                  </label>
+                  <input
+                    type="text"
+                    value={value}
+                    className="border rounded-md px-2 py-1 text-lg"
+                    onChange={(e) =>
+                      setRoom({ ...room, [key]: e.target.value })
+                    }
+                    disabled={key !== "Type" && key !== "Description"}
+                  />
+                </div>
+              ))}
+            <div>
+              <label className="font-bold text-xl">Image</label>
+              <img
+                src={room.image || "https://placehold.co/600x400"}
+                className="py-2 w-40"
+              />
+              <input
+                type="file"
+                className="border rounded-md px-2 py-1 text-sm w-full"
+                ref={imageRef}
+                onChange={() => handleImageUpload()}
+              />
+            </div>
+          </div>
 
-		navigate("/rooms");
-
-		/*
-		* API: Update room details 
-		
-		if (response.ok) {
-			alert("Room updated successfully");
-			navigate("/rooms");
-		} else {
-			alert("Failed to update room");
-			console.error(response);
-		}
-		*/
-	};
-
-	return (
-		<div className="flex flex-col w-full py-4 px-2">
-			<Title title="Room Details" />
-
-			<div className="mx-auto max-w-[700px] w-full">
-				<form
-					className="flex flex-col gap-4 items-center"
-					onSubmit={handleSubmit}
-				>
-					<div className="grid md:grid-cols-2 grid-cols-1 font-play gap-4">
-						<input type="hidden" name="id" value={`${id}`} />
-						{Object.entries(room)
-							.filter(([key]) => key !== "image")
-							.map(([key, value]) => (
-								<div key={key} className="flex flex-col">
-									<label className="font-bold text-xl">
-										{key.charAt(0).toUpperCase() + key.slice(1)}
-									</label>
-									<input
-										type={`${key === "type" ? "text" : "number"}`}
-										value={value}
-										className="border rounded-md px-2 py-1 text-lg"
-										name={key}
-										onChange={(e) =>
-											setRoom({ ...room, [key]: e.target.value })
-										}
-									/>
-								</div>
-							))}
-						<div>
-							<label className="font-bold text-xl">Image</label>
-							<img src={room.image} className="py-2 w-40" />
-							<input
-								type="file"
-								className="border rounded-md px-2 py-1 text-sm w-full"
-								ref={imageRef}
-								onChange={() => handleImageUpload()}
-							/>
-						</div>
-					</div>
-
-					<Button color="green" text="Save Room" type="submit" />
-				</form>
-			</div>
-		</div>
-	);
+          <Button color="green" text="Save Room" type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default RoomDetails;
