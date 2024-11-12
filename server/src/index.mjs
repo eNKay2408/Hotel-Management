ee43166e14f24c97edea4d5d7a67a994178b34aa
eNKay <nguyenkhai24082408@gmail.com>
2024-11-13 01:29:00 +0700

    refactor: add /api prefix to all endpoints
    - Updated index.mjs to add /api prefix to room, booking, and invoice routes

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,38 +1,38 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import RoomRoutes from "./routes/room.mjs";
-import BookingRotes from "./routes/booking.mjs";
-import InvoiceRoutes from "./routes/invoice.mjs";
-import connection from "./database/connectSQL.mjs";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import RoomRoutes from './routes/room.mjs';
+import BookingRotes from './routes/booking.mjs';
+import InvoiceRoutes from './routes/invoice.mjs';
+import connection from './database/connectSQL.mjs';
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.static(`client/public`));
 app.use(express.json());
 
 connection
-    .connect()
-    .then(() => {
-        console.log("Connected to SQL Server");
-    })
-    .catch((err) => {
-        console.log("Error connecting to SQL Server", err);
-    });
+  .connect()
+  .then(() => {
+    console.log('Connected to SQL Server');
+  })
+  .catch((err) => {
+    console.log('Error connecting to SQL Server', err);
+  });
 
-app.use("/rooms", RoomRoutes);
-app.use("/bookings", BookingRotes);
-app.use("/invoices", InvoiceRoutes);
-app.use(cors());
+app.use('/api/rooms', RoomRoutes);
+app.use('/api/bookings', BookingRotes);
+app.use('/api/invoices', InvoiceRoutes);
 
-const HOST = process.env.BACKEND_HOSTNAME || "localhost";
+const HOST = process.env.BACKEND_HOSTNAME || 'localhost';
 const PORT = process.env.BACKEND_PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
 
-app.get("/", (req, res) => {
-    res.send("Back-end server is running");
+app.get('/', (req, res) => {
+  res.send('Back-end server is running');
 });
