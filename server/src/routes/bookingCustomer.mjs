ef2b440a9525f7a49126cacd714c72e318eb672a
Sciderus <huydska11@gmail.com>
2024-11-16 17:50:37 +0700

    export BookingCustomers routes

--- server/src/routes/bookingCustomer.mjs
+++ server/src/routes/bookingCustomer.mjs
@@ -1,5 +1,8 @@
 import { BookingController } from '../controllers/BookingController.mjs';
+import { Router } from 'express';
 
 const router = Router();
 
 router.post('/:id', BookingController.getAllCustomersInBooking);
+
+export default router;
