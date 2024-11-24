d152a7311dffce5d87256e687ce6cf6c1e879c95
eNKay <nguyenkhai24082408@gmail.com>
2024-11-24 18:20:00 +0700

    style: improve responsive design for Table component and apply changes to OccupancyReport
    - Updated Table.jsx to enhance responsive design
    - Applied responsive Table component in OccupancyReport.jsx

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,42 +1,37 @@
 const Table = ({ header, body, color }) => {
-	return (
-		<table className="table-auto text-center w-full">
-			<thead className="table-header-group md:text-xl text-lg">
-				<tr className="table-row">
-					<th
-						key="No"
-						className={`table-cell border h-12 ${
-							color === "red" ? "bg-red" : `bg-${color}`
-						} px-1`}
-					>
-						No
-					</th>
-					{header.map((title, index) => (
-						<th
-							key={index}
-							className={`table-cell border ${
-								color === "red" ? "bg-red" : `bg-${color}`
-							} px-1`}
-						>
-							{title}
-						</th>
-					))}
-				</tr>
-			</thead>
-			<tbody className="md:text-lg text-md">
-				{body.map((row, index) => (
-					<tr key={index}>
-						<td className="border px-1">{index + 1}</td>
-						{row.map((cell, index) => (
-							<td key={index} className="border px-1">
-								{cell}
-							</td>
-						))}
-					</tr>
-				))}
-			</tbody>
-		</table>
-	);
+  return (
+    <table className="table-auto text-center w-full font-play">
+      <thead className="table-header-group md:text-xl text-lg">
+        <tr className="table-row">
+          <th
+            key="No"
+            className={`table-cell border h-12 ${
+              color === 'red' ? 'bg-red' : `bg-${color}`
+            } px-2`}
+          >
+            No
+          </th>
+          {header.map((title, index) => (
+            <th key={index} className={`table-cell border bg-${color} px-2`}>
+              {title}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className="md:text-lg text-base">
+        {body.map((row, index) => (
+          <tr key={index}>
+            <td className="border p-1">{index + 1}</td>
+            {row.map((cell, index) => (
+              <td key={index} className="border px-1">
+                {cell}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
