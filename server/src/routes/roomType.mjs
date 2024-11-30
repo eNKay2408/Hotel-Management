e593b4f4e295db1329525b1ba398f8524b0579a7
Sciderus <huydska11@gmail.com>
2024-11-30 20:51:38 +0700

    add post, put, delete

--- server/src/routes/roomType.mjs
+++ server/src/routes/roomType.mjs
@@ -5,5 +5,8 @@ const router = Router();
 
 // get all room types
 router.get('/', RoomController.getAllRoomsTypes);
+router.post('/', RoomController.createNewRoomType);
+router.put('/:id', RoomController.updateRoomType);
+router.delete('/:id', RoomController.deleteRoomType);
 
 export default router;
