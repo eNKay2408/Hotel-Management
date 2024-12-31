d41ace7b4dbc2f76f6f11304a81bc84fee7e9df3
Ma Cat Huynh <uynhhuc810@gmail.com>
2024-12-31 18:27:27 +0700

    fixes the test files of invoiceControllerTest

--- server/src/database/ConnectSQL.mjs
+++ server/src/database/ConnectSQL.mjs
@@ -1,23 +1,23 @@
-import dotenv from "dotenv";
-import sql from "mssql";
+import dotenv from 'dotenv';
+import sql from 'mssql';
 
 dotenv.config();
 
 const sqlConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    server: process.env.DB_SERVER,
-    driver: "mssql",
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000,
-    },
-    options: {
-        encrypt: true, // for azure
-        trustServerCertificate: true, // change to true for local dev / self-signed certs
-    },
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  server: process.env.DB_SERVER,
+  driver: 'mssql',
+  pool: {
+    max: 20,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true, // change to true for local dev / self-signed certs
+  },
 };
 
 const connection = new sql.ConnectionPool(sqlConfig);
